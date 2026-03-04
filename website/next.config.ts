180ee01b85c6034df556087f4203ac0aa8125882
Mateo-Piedra22 <piedrabuena.mateo03@gmail.com>
2026-03-04 11:14:28 -0300

    feat: add Next.js configuration to disable poweredByHeader and allow server actions from a specific origin.

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['irontrain.motiona.xyz']
+    }
+  }
 };
 
 export default nextConfig;
